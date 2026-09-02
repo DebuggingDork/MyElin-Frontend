ef336be72b5196f2805f3ee8e8354b5b5dc40c36
shivvv_sai_18_07 <shivasai8956@gmail.com>
2026-09-02 14:24:24 +0530

    chore(test): add vitest test harness

---
+++ test/setup.ts
@@ -0,0 +1,15 @@
+// Vitest setup file
+import { beforeAll, afterEach, afterAll } from 'vitest';
+
+// Add any global test setup here
+beforeAll(() => {
+  // Setup code before all tests
+});
+
+afterEach(() => {
+  // Cleanup after each test
+});
+
+afterAll(() => {
+  // Cleanup after all tests
+});
